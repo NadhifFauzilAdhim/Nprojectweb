a8f100c442e87848f011aee686785131c4a79109
NadhifFauzilAdhim <nadya15a3@gmail.com>
2023-08-06 22:04:48 +0700

    add live chat and fix bug

--- php/mail.php
+++ php/mail.php
@@ -1,16 +1,35 @@
 <?php
-  if(isset( $_POST['name']))
-  $name = $_POST['name'];
-  if(isset( $_POST['email']))
-  $email = $_POST['email'];
-  if(isset( $_POST['message']))
-  $message = $_POST['message'];
-  if(isset( $_POST['subject']))
-  $subject = $_POST['subject'];
 
-  $content="From: $name \n Email: $email \n Message: $message";
-  $recipient = "nadya15a3@gmail.com";
-  $mailheader = "From: $email \r\n";
-  mail($recipient, $subject, $content, $mailheader) or die("Error!");
-  echo "Email sent!";
-?>
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+require "../vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.zoho.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "nadhiffauzila@zohomail.com";
+$mail->Password = "nadia20a3";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("analyticgames@gmail.com", "Nadhif");
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+header("Location: sent.html");
